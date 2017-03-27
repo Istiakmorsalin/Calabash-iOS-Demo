eb866fb980d45a50eb6d4b7535af8ce61dc12f20
IstiakMorsalin <anik.cse@gmail.com>
2017-03-27 15:04:50 +0600

    project file change

--- Calabash-iOS-Demo/FirstTableViewCell.swift
+++ Calabash-iOS-Demo/FirstTableViewCell.swift
@@ -12,15 +12,15 @@ class FirstTableViewCell: UITableViewCell {
 
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var label: UILabel!
+    //let disposeBag = DisposeBag()
+
+   
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
+    
+        
+    
 }
